51641ef73dd09c8f281bc9795263181a492a03b1
trallgorm <the_russian_vsn@hotmail.com>
2017-12-12 01:19:35 -0500

    Added weighted stats and commandline options

--- kNNpredict.py
+++ kNNpredict.py
@@ -6,13 +6,27 @@ import sys
 k = int(math.sqrt(len(ScraperDAO.fighterDifferencesAndResultsList)))
 
 def testTopStats():
-    datalists = kNNshared.splitDataset(0.66,ScraperDAO.fighterDifferencesAndResultsList)
+    datalists = kNNshared.splitDataset(0,ScraperDAO.fighterDifferencesAndResultsList)
 
     correctGuesses = 0
+    i=0
     for fight in datalists["TestList"]:
-        if fight["Result"] == predictOutcomeTopStats(fight,1000)[0]:
+        i+=1
+        if fight["Result"] == predictOutcomeTopStats(fight,100)[0]:
             correctGuesses += 1
-        print(correctGuesses)
+        print("Tested: " + str(i) + "/" + str(len(datalists["TestList"])) + ", Correct: " + str(round(100*correctGuesses / i,2)) + "%")
+
+    return ((correctGuesses / len(datalists["TestList"])) * 100)
+
+def testWeightedStats():
+    datalists = kNNshared.splitDataset(0,ScraperDAO.fighterDifferencesAndResultsList)
+    i=0
+    correctGuesses = 0
+    for fight in datalists["TestList"]:
+        i+=1
+        if fight["Result"] == predictOutcomeWeighted(fight)[0]:
+            correctGuesses += 1
+        print("Tested: " +str(i) +"/"+str(len(datalists["TestList"])) + ", Correct: " + str(round(100*correctGuesses/i,2)) +"%" )
 
     return ((correctGuesses / len(datalists["TestList"])) * 100)
 
@@ -28,6 +42,26 @@ def predictOutcomeTopStatsWrapper(fighterNameA, fighterNameB, amountOfTopStats):
 def predictOutcomeAllStatsWrapper(fighterNameA, fighterNameB):
     return predictOutcomeTopStatsWrapper(fighterNameA, fighterNameB, sys.maxint)
 
+def predictOutcomeWeightedWrapper(fighterNameA, fighterNameB):
+    result = predictOutcomeWeighted(ScraperDAO.getDifferencesBetweenFighters(fighterNameA, fighterNameB))
+    if result[0] == "Win":
+        print(fighterNameA + " wins with " + result[1] + "% accuracy")
+    else:
+        print(fighterNameB + " wins with " + result[1] + "% accuracy")
+
+#Weighs the importance of a prediction based on how accurate the stats are
+def predictOutcomeWeighted(fighterDifferences):
+    winTotal = 0.0
+    lossTotal = 0.0
+    for statArray in kNNshared.readTopStatsFromFile(1000):
+        if kNNshared.makePrediction(fighterDifferences,statArray[0],ScraperDAO.fighterDifferencesAndResultsList, k)=="Win":
+            winTotal+=(1*(statArray[1]/100))
+        else:
+            lossTotal+=(1*(statArray[1]/100))
+    if winTotal > lossTotal:
+        return ("Win", str(winTotal * 100 / (winTotal + lossTotal)))
+    else:
+        return ("Loss", str(lossTotal * 100 / (winTotal + lossTotal)))
 
 #Predicts the outcome of a fight given two fighters based on the top X stats rated by accuracy
 def predictOutcomeTopStats(fighterDifferences,amountOfTopStats):
@@ -43,3 +77,15 @@ def predictOutcomeTopStats(fighterDifferences,amountOfTopStats):
         return("Win",str(win*100/(win+loss)))
     else:
         return("Loss",str(loss*100/(win+loss)))
+
+if sys.argv[1] == "-testWeighted":
+    testWeightedStats()
+elif sys.argv[1] == "-testTop":
+    testTopStats()
+else:
+    if "-top" in sys.argv:
+        predictOutcomeTopStatsWrapper(sys.argv[1],sys.argv[2],int(sys.argv[4]))
+    else:
+        predictOutcomeWeightedWrapper(sys.argv[1],sys.argv[2])
+
+
